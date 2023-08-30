108b5a3e54a22c61ab879bb7d604e116482fff6a
desipossa <c1jang@hanmail.net>
2023-08-30 11:48:10 +0900

    아이구 어렵다...

--- js/custom.js
+++ js/custom.js
@@ -13,13 +13,23 @@ const PAGE_BG = document.querySelector('.pagenation .bg');
 const LIST_NAME = ['intro', 'portfolio01', 'portfolio02', 'portfolio03']
 const LIST_COLOR = ['#fff', '#f00', 'tomato', '#333'];
 
+const SLIDE = document.querySelector('#slide_move');
+const SLIDE_ITM = document.querySelectorAll('#slide_move .slide');
+
 new fullpage('#main', {
-    anchors: ['p01', 'p02', 'p03', 'footer'],
+    anchors: ['p01', 'p02', 'p03', 'p04', 'p05', 'footer'],
     css3: false,
     scrollOverflow: false, //line-height: 1에서 font-size가 box를 초과할 때 스크롤이 생기는 초기값을 false로 설정함.
 
+
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+
+    controlArrows: false, //슬라이드 화살표 숨김
+    loopHorizontal: false, //슬라이드 반복 멈춤
+
+
     afterLoad: function (origin, destination, direction, trigger) {
-        console.log(destination.index, direction);
+        // console.log(destination.index, direction);
         ASIDE.forEach(it => it.classList.remove('on'));
         ASIDE[destination.index].classList.add('on');
         SECTION.forEach(it => it.classList.remove('on'));
@@ -39,8 +49,51 @@ new fullpage('#main', {
         if (direction == 'up') {
             HEADER.classList.remove('on');
         }
+
+        // 5번째 페이지 슬라이드에 on을 붙임.
+        if (destination.index == 4) {
+            SLIDE_ITM[0].classList.add('on');
+        } else {
+            SLIDE_ITM[0].classList.remove('on');
+        }
+        // else {
+        //     SLIDE_ITM.forEach(it => it.classList.remove('on'));
+        // }
+    },
+
+    afterSlideLoad: function (section, origin, destination, direction, trigger) {
+        // console.log(destination.index, SLIDE_ITM[0]);
+
+        SLIDE_ITM.forEach(it => it.classList.remove('on'));
+        SLIDE_ITM[destination.index].classList.add('on');
     },
 });
+
+
+
+//슬라이드에 훨이벤트 달기...
+
+SLIDE.addEventListener('wheel', (e) => {
+
+    console.log(e, e.deltaY); // e.deltaY 100, -100
+
+    if (e.deltaY > 0) {
+        fullpage_api.moveSlideRight();
+    } else {
+        fullpage_api.moveSlideLeft();
+    }
+
+})
+
+// jquery 로 만들 때...
+// $("#slide").on("mousewheel", function (e) {
+//     if (e.originalEvent.wheelDelta / 120 > 0) {
+//         fullpage_api.moveSlideLeft();
+//     }
+//     else {
+//         fullpage_api.moveSlideRight();
+//     }
+// });
 
 
 COVER_BTN.addEventListener('click', function (e) {
